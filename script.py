28d5f643f3dac0ee8f4acf583d7113541e866b3f
zefirlover <71660586+zefirlover@users.noreply.github.com>
2023-02-24 10:40:41 +0200

    Update if statement

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ async def main():
         # get_chat_history(chat_id, 10) get last 10 messages
         async for messages in app.get_chat_history(chat_id, 15):
             print(messages.reactions)
-            if messages.reactions is None or messages.reactions == [types.Reaction(count=int,emoji=reaction)] or messages.reactions != [types.Reaction(count=int,chosen_order=0,emoji=reaction)]:
+            if messages.reactions is None or messages.reactions == [types.Reaction(count=int,emoji=reaction)]:
                 await app.send_reaction(chat_id, messages.id, reaction)
 
 loop = asyncio.get_event_loop()
